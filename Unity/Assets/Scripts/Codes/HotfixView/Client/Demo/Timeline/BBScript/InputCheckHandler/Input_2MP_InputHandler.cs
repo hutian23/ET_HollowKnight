8dd689637dfa7f8347d1ad91b2d7db70114d381d
hutian123 <2504676118@qq.com>
2024-11-15 12:15:56 +0800

    添加了Mai_2c行为，可以将敌人浮空

---
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/Timeline/BBScript/InputCheckHandler/Input_2MP_InputHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace ET.Client
+{
+    public class Input_2MP_InputHandler : BBInputHandler
+    {
+        public override string GetInputType()
+        {
+            return "2MP";
+        }
+        
+        public override async ETTask<Status> Handle(Unit unit, ETCancellationToken token)
+        {
+            InputWait inputWait = BBInputHelper.GetInputWait(unit);
+            
+            //1. Wait Down
+            // WaitInput wait = await inputWait.Wait(OP: BBOperaType.DOWN | BBOperaType.DOWNLEFT | BBOperaType.DOWNRIGHT, FuzzyInputType.OR, () =>
+            // {
+            //     
+            // });
+            
+            await ETTask.CompletedTask;
+            return Status.Success;
+        }
+    }
+}
