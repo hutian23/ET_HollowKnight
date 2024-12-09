f45cac81ce495b8d887093afbaf2a6b9b7922915
hutian123 <2504676118@qq.com>
2024-12-09 10:01:52 +0800

    SquatHold_InputHandler

---
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/FightingGame/BBScript/InputCheckHandler/Input_SquatHold_Handler.cs
@@ -0,0 +1,20 @@
+﻿namespace ET.Client
+{
+    public class Input_SquatHold_Handler : InputHandler
+    {
+        public override string GetHandlerType()
+        {
+            return "SquatHold";
+        }
+
+        public override string GetBufferType()
+        {
+            return "SquatHold";
+        }
+
+        public override long Handle(InputWait self)
+        {
+            return self.IsPressing(BBOperaType.DOWN) || self.IsPressing(BBOperaType.DOWNLEFT) || self.IsPressing(BBOperaType.DOWNRIGHT) ? self.GetBuffFrame(7) : -1;
+        }
+    }
+}
